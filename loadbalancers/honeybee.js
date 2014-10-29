a6099fb2240a986f1a217b0ec5de9d18bce2cabb
bsaideepak <saideepak09@gmail.com>
2014-10-29 02:20:21 -0700

    honeybee.js changes

--- loadbalancers/honeybee.js
+++ loadbalancers/honeybee.js
@@ -2,73 +2,60 @@
  * Author: Sai
  */
 
-var servers = require("../util/servers");
+var common = require("../util/common");
 
 var serverPointer=0;
 
 function allocateServer(callback,conf,req)
 {
-	if (!conf.server.serverNodes.length<=0)
-	{
-		servers.findAllServers(function(err,result){
+	
+	common.findAvailableServersWithResources(function(err,docs){
 				
-			if(err){
-				console.log("Error.");
-			}
-			else{
-
-				temp = result;
-
-				result.toArray(function(err,docs){
-
-					if(!docs.length==0)
-					{
-						var no_of_requests = [];
-						var serverId_list = [];
-						var resourceCount_list = [];
-
-						if(docs[0].resourceCount > 0){
-							var choice = docs[0].serverId;
-						}
-
-						
-						for(var i=0; i<docs.length;i++)
-						{
-							no_of_requests[i] = docs[i].liveReq;
-							serverId[i] = docs[i].serverId;
-
-							if(docs[i].resourceCount > 0){
-								resourceCount_list[i] = 1;
-							}
-
-
-							if (i>0) {
-
-								if (no_of_requests[i]<no_of_requests[i-1] && resourceCount_list[i]==1) {
-
-									choice = serverId_list[i];
-								}
-							}
-							
-						}
-
-						serverPointer = choice;
-								
-						callback(serverPointer,null);
-					}
-
-					else{
-						
-						callback(null,new Error("No Servers found to handle Requests"));
-					}
-				
-				});
-					
-				}
-				
-			});
+		if(err){
+			console.log("Error.");
+		}
 		
-	}
+		else{
+
+			if(!docs.length==0)
+			{
+				var no_of_requests = [];
+				var serverId_list = [];
+		
+				for(var i=0; i<docs.length;i++)
+				{
+					no_of_requests[i] = docs[i].liveReq;
+					serverId[i] = docs[i].serverId;
+
+
+					if (i>0) {
+
+						if (no_of_requests[i]<no_of_requests[i-1]) {
+
+							choice = serverId_list[i];
+						}
+					}
+					else{
+						choice = serverId_list[i];
+					}
+							
+				}
+
+				serverPointer = choice;
+								
+				callback(null , serverPointer);
+			}
+
+			else{
+				console.log("No servers available to service requests.");
+				callback(null, new Error("No servers available to service requests."));
+			}
+					
+		}
+				
+	},conf,quantity);
+		
 }
+
 
 exports.allocateServer = allocateServer;
