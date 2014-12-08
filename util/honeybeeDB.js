4c3e0e6585c0d7b0b8036999b8d071ea054f284f
leonacherla <leonacherla@gmail.com>
2014-12-08 15:19:15 -0800

    Indentation update

--- util/honeybeeDB.js
+++ util/honeybeeDB.js
@@ -16,17 +16,17 @@ function incrementLiveReqCount(serverId){
 		}
 		else{
 			dbc = coll;
-				dbc.findAndModify({query: {"serverId": serverId },update: { $inc: { "liveReq": 1 } }, upsert: true },function(err,result){
-		if(err){
-			console.log("Error WHile Updating.");
-			//common.closeConnection(db);
-		}
-		
-		else{
-			console.log("Recorded Updated.");
-			//common.closeConnection(db);
-		}
-	});
+			dbc.findAndModify({query: {"serverId": serverId },update: { $inc: { "liveReq": 1 } }, upsert: true },function(err,result){
+				if(err){
+					console.log("Error WHile Updating.");
+					//common.closeConnection(db);
+				}
+
+				else{
+					console.log("Recorded Updated.");
+					//common.closeConnection(db);
+				}
+			});
 		}
 	},collectionName);
 
@@ -43,18 +43,18 @@ function decrementLiveReqCount(serverId){
 		}
 		else{
 			dbc = coll;
-	dbc.findAndModify({query: {"serverId": serverId },update: { $inc: { "liveReq": -1 } }, upsert: true },function(err,result){
+			dbc.findAndModify({query: {"serverId": serverId },update: { $inc: { "liveReq": -1 } }, upsert: true },function(err,result){
 
-		if(err){
-			console.log("Error WHile Updating.");
-			//common.closeConnection(db);
-		}
-		else{
-			console.log("Recorded Updated.");
-			//common.closeConnection(db);
-		}
-	});
-			
+				if(err){
+					console.log("Error WHile Updating.");
+					//common.closeConnection(db);
+				}
+				else{
+					console.log("Recorded Updated.");
+					//common.closeConnection(db);
+				}
+			});
+
 		}
 	},collectionName);
 
