6963eca2936da4ee8faa9f02b5edd3764d0c84af
leonacherla <leonacherla@gmail.com>
2014-12-08 15:17:35 -0800

    Indentation update

--- util/MongoDBConnectionPool.js
+++ util/MongoDBConnectionPool.js
@@ -11,28 +11,28 @@ var fs = require("fs");
 var conf = (JSON.parse(fs.readFileSync("./config/conf.json", "utf8")));
 
 
-// Initialize connection once
+//Initialize connection once
 MongoClient.connect("mongodb://localhost:27017/ldb?maxPoolSize=300", function(err, database) {
 	if(err){
-    	console.log("Error Creating MongoDB Connection Pool: "+err);
-  	}
+		console.log("Error Creating MongoDB Connection Pool: "+err);
+	}
 	else{
-  		db = database;
+		db = database;
 	}
 });
 
 function getConnection(callback,collectionName){
 
-  db.collection(collectionName, function(err,coll){
+	db.collection(collectionName, function(err,coll){
 
-    if(err){
-      console.log("Error Connecting to collection: "+err);
-    }
-    else{
-      callback(null,coll);
-    }
+		if(err){
+			console.log("Error Connecting to collection: "+err);
+		}
+		else{
+			callback(null,coll);
+		}
 
-  });
+	});
 }
 
 exports.getConnection = getConnection;
