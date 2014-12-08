99991363b5fb4054eff9bd172f252c53be88da98
leonacherla <leonacherla@gmail.com>
2014-12-08 15:20:35 -0800

    Indentation update

--- util/psoDB.js
+++ util/psoDB.js
@@ -11,7 +11,7 @@ var mongo = require("../util/MongoDBConnectionPool");
 
 
 function logT1(callback,json){
-	
+
 	if(json.requestId && json.t1){
 
 		mongo.getConnection(function(err,coll){
@@ -20,25 +20,25 @@ function logT1(callback,json){
 			}
 			else{
 				dbc = coll;
-				
+
 				dbc.insert({'requestId':json.requestId, 't1': json.t1, 't2': "", 'tRes': 0},function (err,result){
-			
-			if(err){
-				console.log(err);
-				//common.closeConnection(db);
-				callback(err,null);
-			}
-			
-			else{
-				
-				var status = "Successfully Inserted";
-				//common.closeConnection(db);
-				console.log("Operation Successful.");
-				callback(err,status);
-			}
-		
-		});
-				
+
+					if(err){
+						console.log(err);
+						//common.closeConnection(db);
+						callback(err,null);
+					}
+
+					else{
+
+						var status = "Successfully Inserted";
+						//common.closeConnection(db);
+						console.log("Operation Successful.");
+						callback(err,status);
+					}
+
+				});
+
 			}
 		},collectionName);
 	}
@@ -54,25 +54,25 @@ function findRequestDetailsById(callback,requestId){
 		}
 		else{
 			dbc = coll;
-			
+
 			dbc.find({"requestId": requestId},function(err,res){
-		
-		if(err){
-			console.log("No Server exists.");
-			//common.closeConnection();
-		}
-						
-		else{
-			//common.closeConnection();
-			callback(err,res);
-		}
-			
-	});
-		
+
+				if(err){
+					console.log("No Server exists.");
+					//common.closeConnection();
+				}
+
+				else{
+					//common.closeConnection();
+					callback(err,res);
+				}
+
+			});
+
 		}
 	},collectionName);
 }
-	
+
 exports.findRequestDetailsById = findRequestDetailsById;
 
 function logT2AndResponceTime(json){
@@ -85,19 +85,19 @@ function logT2AndResponceTime(json){
 			}
 			else{
 				dbc = coll;
-				
+
 				dbc.findAndModify({query: {"requestId": json.requestId },update: { $set: { "t2": json.t2 , 'tRes': (json.t2 - json.t1)} }, upsert: true },function(err,result){
 
-			if(err){
-				console.log(err);
-				//db.close();
-			}
-			else{
-				console.log("Successfully Updated.");
-				//db.close();
-			}
-		});
-				
+					if(err){
+						console.log(err);
+						//db.close();
+					}
+					else{
+						console.log("Successfully Updated.");
+						//db.close();
+					}
+				});
+
 			}
 		},collectionName);
 	}
@@ -111,35 +111,35 @@ exports.logT2AndResponceTime = logT2AndResponceTime;
 
 function calculateTimeAverage(callback,json){
 
-var collName = "servers";
-	
+	var collName = "servers";
+
 	var temp = 0;
 	var count++;
-	
+
 	mongo.getConnection(function(err,coll){
 		if(err){
 			console.log("Error: "+err);
 		}
 		else{
 			dbc = coll;
-			
+
 			dbc.find({'tRes' : { $gt : 0 }}, function(err,result){
-		//Aggregate and find average responce time.
-		
-		result.toArray(function(err,docs){
-			
-			if(docs.length!= 0){
-				temp = temp + docs[i].tRes;
-				count++;
-			}
-			
-			
-		});
-		
-		tAvg = temp/count;
-		callback(err,tAvg);
-		
-	});
+				//Aggregate and find average responce time.
+
+				result.toArray(function(err,docs){
+
+					if(docs.length!= 0){
+						temp = temp + docs[i].tRes;
+						count++;
+					}
+
+
+				});
+
+				tAvg = temp/count;
+				callback(err,tAvg);
+
+			});
 		}
 	},collName);
 }
