3df45e56dee57b09402f21825526abce06cab3d8
leonacherla <leonacherla@gmail.com>
2015-03-29 19:45:25 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'CMPE 281 HW 1 Cloud Load Balancer - TEAM 21'});
+  res.render('index', { title: 'CMPE 281 Cloud Load Balancer - TEAM 21'});
 };
 
 exports.ping = function(req, res){
